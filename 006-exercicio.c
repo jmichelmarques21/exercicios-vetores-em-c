cc7cc714affb806d03cd898df782622aa8d30046
Jean Marques <101370158+jmichelmarques21@users.noreply.github.com>
2023-10-25 00:57:29 -0300

    Update 006-exercicio.c

--- 006-exercicio.c
+++ 006-exercicio.c
@@ -1,3 +1,6 @@
+/* Exercício 06 - Escreva um programa que calcule o produto escalar entre dois vetores de inteiros de tamanho igual a 5. 
+Exemplo: {0, 2, 4, 6, 8} x {1, 3, 5, 7, 9} = 0 * 1 + 2 * 3 + 4 * 5 + 6 * 7 + 8 * 9 = 140. */
+
 #include<stdio.h>
 #include<locale.h>
 
@@ -12,7 +15,7 @@ int main(){
 		produto += vetA[i] * vetB[i]; 
 	}
 	
-	printf("\nO produto escalar entre esses dois vetores �: %d", produto);
+	printf("\nO produto escalar entre esses dois vetores é: %d", produto);
 	
 	
 	return 0;
